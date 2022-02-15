0afeb8579160e7d2edfee11fbf9d948c03301244
Thibaud Ros <thibaud.ros@gmail.com>
2022-02-15 11:20:17 +0100

    modif input messages post pour text area, correction bug si pas d'image poster

--- backend/controllers/postsControllers.js
+++ backend/controllers/postsControllers.js
@@ -9,17 +9,34 @@ exports.showAllPosts = (req, res, next) => {
         .then(posts => res.status(200).json(posts))
         .catch(error => res.status(400).json({ error }));
 };
+
 exports.createOnePost = (req, res, next) => {
     const post = JSON.parse(req.body.post);
-    const postToCreate = Post.build({
-        nom_auteur: post.nom_auteur,
-        prenom_auteur: post.prenom_auteur,
-        poste_auteur: post.poste_auteur,
-        user_id: post.user_id,
-        messages: post.messages,
-        urlMedia: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
-    postToCreate.save()
-        .then(() => res.status(201).json({ message: 'Post enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
+    if (req.file) {
+        const postToCreate = Post.build({
+            nom_auteur: post.nom_auteur,
+            prenom_auteur: post.prenom_auteur,
+            poste_auteur: post.poste_auteur,
+            user_id: post.user_id,
+            messages: post.messages,
+            urlMedia: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        });
+        postToCreate.save()
+            .then(() => res.status(201).json({ message: 'Post enregistré !' }))
+            .catch(error => res.status(400).json({ error }));
+    } else {
+        const postToCreate = Post.build({
+            nom_auteur: post.nom_auteur,
+            prenom_auteur: post.prenom_auteur,
+            poste_auteur: post.poste_auteur,
+            user_id: post.user_id,
+            messages: post.messages,
+            urlMedia: null
+        });
+        postToCreate.save()
+            .then(() => res.status(201).json({ message: 'Post enregistré !' }))
+            .catch(error => res.status(400).json({ error }));
+    }
+
+
 };
